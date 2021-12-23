ed251e6e4d56821799acb36ae5002bae2c1fe22d
infestedmind <myinfestedmind@gmail.com>
2021-12-24 00:39:33 +0300

    temp

---
+++ config.py
@@ -0,0 +1,8 @@
+from starlette import config
+from starlette.config import Config
+
+config = Config(".env")
+
+DATABASE: str = config("DATABASE")
+USER: str = config("USER")
+PASSWORD: str = config("PASSWORD")
